bce67fa19725896da34c9520763f8114722318d7
Priscila.Hovaneso <pehovaneso@hotmail.com>
2025-08-01 16:50:17 -0300

    App.css modificaciones

--- src/Components/ContactItem/ContactItem.jsx
+++ src/Components/ContactItem/ContactItem.jsx
@@ -7,8 +7,8 @@ const ContactItem = ({ contact }) => {
      <Link to={`/contact/${contact.id}/messages`}className="contact-item">   
       <h2>{contact.name}</h2>
       <img src= {contact.avatar} alt={contact.name}className="avatar" width={100} />
-      <span className="contact-lastConnection">Ayer{contact.lastConnection}</span>
-      <span className="contact-connectionStatus">Desconectado {contact.connectionStatus}</span>
+      <span class="contact-lastConnection">Ayer{contact.lastConnection}</span>
+      <span class="contact-connectionStatus">Desconectado {contact.connectionStatus}</span>
 
     </Link>
   );
